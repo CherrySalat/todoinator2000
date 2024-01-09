ff1e2c7e32ccd14d19c681eb071611cfc9cd4f9e
CherrySalat <trakhimov01@mail.ru>
2024-01-10 00:12:29 +0400

    add some new derictories and split main.go to few little files

---
+++ exampleData.go
@@ -0,0 +1,14 @@
+package main
+
+var Tasks = []Task{
+	Task{
+		Id:          int(1),
+		Title:       "Buy some bread",
+		Description: "1. Go to market\n2.take some bread\n3.Buy it",
+	},
+	Task{
+		Id:          2,
+		Title:       "Be cool",
+		Description: "1. Put on black glasses 2. Kick some asses 3. Drink beer.",
+	},
+}
